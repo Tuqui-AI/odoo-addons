a6ea3bc882b4c33d0aefc7fc6130997464bdbeb1
Lara <lav@adhoc.inc>
2026-06-18 08:54:05 -0300

    [IMP] tuqui_mcp: add images key to manifest for cover image

--- tuqui_mcp/__manifest__.py
+++ tuqui_mcp/__manifest__.py
@@ -6,6 +6,7 @@
     "author": "Tuqui-AI",
     "website": "https://tuqui.com",
     "license": "LGPL-3",
+    "images": ["static/description/portada.png"],
     "depends": ["base"],
     "data": [
         "data/tuqui_mcp_data.xml",
